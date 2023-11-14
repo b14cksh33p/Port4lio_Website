79716b4b9a2b8e1bbd4452a4cc42557a9c8eaef1
LouisaRSantos <louisasantosl12@gmail.com>
2023-11-14 09:46:36 +0800

    Enhance Login Page

--- blacksheep-portfolio-website/src/pages/login/login.js
+++ blacksheep-portfolio-website/src/pages/login/login.js
@@ -1,35 +1,53 @@
 import './login.css';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Login() {
+  
+  const [emailLogin, setEmail] = useState('');
+  const [passwordLogin, setPassword] = useState('')
+
+  const handleLogin = () => {
+    if (emailLogin && passwordLogin && emailLogin.includes('@')) {
+      alert('Login Successful!');
+      setEmail('');
+      setPassword('');
+    } else {
+      alert('Login not Successful');
+      setEmail('');
+      setPassword('');
+    }
+  };
+
     return (
-      <div className='wrapper'>
-        <div className='container main'>
+      <div className='wrapper-login'>
+        <div className='container-login main'>
           <div className='row'>
             <div className='col-md-6 side-image'>
             </div>
             
             <div className='col-md-6 right'>
 
-              <div className='input-box'>
-                <div className='port4lio-text'>PORT4LIO</div>
-                <div className='welcome-text'>Welcome back, User!</div>
-                <div className='input-field'>
-                  <input type='text' className='input' id='email-input-login' required autoComplete='off'></input>
-                  <label for="email-input-login">Email</label> 
+              <div className='input-box-login'>
+                <div className='port4lio-text-login'>PORT4LIO</div>
+                <div className='welcome-text-login'>Welcome back, User!</div>
+                <form onSubmit={handleLogin}>
+                <div className='input-field-login'>
+                  <input type='text' className='input-login' id='email-input-login' required autoComplete='off' value={emailLogin} onChange={(e) => setEmail(e.target.value)}></input>
+                  <label htmlFor="email-input-login" className='label-login'>Email</label> 
                 </div>
-                <div className='input-field'>
-                  <input type='password' className='input' id='password-input-login' required></input>
-                  <label for="password-input-login">Password</label> 
+                <div className='input-field-login'>
+                  <input type='password' className='input-login' id='password-input-login' required value={passwordLogin} onChange={(e) => setPassword(e.target.value)}></input>
+                  <label htmlFor="password-input-login" className='label-login'>Password</label> 
                 </div>
-                <div className='input-field'>
+                <div className='input-field-login'>
                   <input type='submit' className='submit-btn-login' value='Login'></input>
                 </div>
+                </form>
                 <div className='signin-btn'>
                   <span className='span-login'>Don't have an account? <a href='#'>Sign up here</a></span>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
