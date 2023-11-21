41bea2dc980220bfb9b5153620ac6f511f22f21d
b14sh33p <mjae0109@gmail.com>
2023-11-21 19:32:57 +0800

    firebase fix

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../header/header.js'
+import firebase from '../../firebaseConfig.js';
 
 function Login() {
   
