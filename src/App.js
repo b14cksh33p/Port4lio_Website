bb01488d6c1726d799d3d9537615ccee9171674f
Roderick002 <ballaranroderick@gmail.com>
2023-11-26 19:54:39 +0800

    fix merge conflicts

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from '../src/pages/landing/landing.js';
 import SignupPage from '../src/pages/signup/signup.js';
   import SUPersonal from'../src/pages/signup/page2-personalinfo/personalInfo.js';
-  import PIreq from '../src/pages/signup/page3-preinternreq/preInternreq.js';
-  import PoIreq from '../src/pages/signup/page4-postinternreq/postInterreq.js';
-import LoginPage from '../src/pages/login/login.js';
+
+import LoginPage from '../src/pages/login/login-new.js';
 import StudentPortfolioPage from './pages/studentportfolio/studentportfolio.js';
   import StudentProfilePage from './pages/studentportfolio/studentprofile/studentprofile.js';
 import HomePage from '../src/pages/home/home.js';
@@ -25,8 +24,6 @@ function App() {
         <Route path="/" element={<LandingPage/>} />
         <Route path="/signup/primary-information" element={<SignupPage/>} />
           <Route path="/signup/personal-information" element={<SUPersonal/>} />
-          <Route path="/signup/pre-internship-requirements" element={<PIreq/>} />
-          <Route path="/signup/post-internship-requirements" element={<PoIreq/>} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/student-portfolio" element={<StudentPortfolioPage/>} />
           <Route path="/student-portfolio/profile" element={<StudentProfilePage/>} />
