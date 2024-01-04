b6e3e9674bb115de5bd8941e1f0638f49a9cb924
b14sh33p <mjae0109@gmail.com>
2024-01-04 13:30:33 +0800

    fixed landing syntax issue

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -40,14 +40,14 @@ function Landing() {
                   <p>• Get to know our OJT experience</p>
                   <p>• Get to know our HTEs</p>
                   <div className='button-container'>
-                    <button onClick={()=>{userName=='' ? navigate('/login') : navigate()}}>Login</button>
+                    <button onClick={()=>{userName==='' ? navigate('/login') : navigate()}}>Login</button>
                   </div>
                 </div>
               </div>
               <div className='main-content s3'>
                 <div className='content-text right'>
                 <h4>Start using <span style={{color:'#682c0e'}}>PORT4LIO</span> by creating your free account.</h4>
-                  <button onClick={()=>{userName=='' ? navigate('/signup/primary-information') : navigate()}}>Sign Up</button>
+                  <button onClick={()=>{userName==='' ? navigate('/signup/primary-information') : navigate()}}>Sign Up</button>
                 </div>
                 <div className='content-image'>
                   <img src={Image3} alt='banner'/>
