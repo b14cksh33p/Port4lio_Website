6eaef03ad96eca5705559818d188ae071a3c9ed3
b14sh33p <mjae0109@gmail.com>
2024-01-04 13:10:27 +0800

    navigation fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals'; 
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Router basename="/port4lio-website">
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
   </Router>,
   document.getElementById('root')
